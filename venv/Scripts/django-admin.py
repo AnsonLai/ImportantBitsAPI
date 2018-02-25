617b17d1240250306c71942f42de5a2b27751158
PharaohsVizier <PharaohsVizier@users.noreply.github.com>
2018-02-24 19:58:41 -0800

    Added a Procfile.

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\phara\documents\djangoapi\importantbitsapi\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
